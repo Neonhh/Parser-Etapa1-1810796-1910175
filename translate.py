3c5f23598fb176c2c07360449c0ae9d401363109
Luis Isea <isea.luis.miguel@gmail.com>
2025-07-20 06:37:29 -0400

    Update .gitignore, add README and PDF files, and refactor translate.py

--- translate.py
+++ translate.py
@@ -3,13 +3,13 @@ from parse import SymbolTable, generate_AST
 import ply.yacc as yacc
 from lexer import lexer, tokens
 
+
 def generate_python_file(input_filename, translated_code, variables):
     # Genera el nombre del archivo de salida
     output_filename = f"{input_filename.replace('.imperat', '')}_output.py"
 
     # Definiciones iniciales requeridas
-    preamble = """# Definiciones básicas de lambda cálculo
-Z = lambda g: (lambda x: g(lambda v: x(x)(v)))(lambda x: g(lambda v: x(x)(v)))
+    preamble = """Z = lambda g: (lambda x: g(lambda v: x(x)(v)))(lambda x: g(lambda v: x(x)(v)))
 true = lambda x: lambda y: x
 false = lambda x: lambda y: y
 nil = lambda x: true
@@ -20,29 +20,25 @@ apply = Z(lambda g: lambda f: lambda x: f if x == nil else (g(f(head(x)))(tail(x
 lift_do = lambda exp: lambda f: lambda g: lambda x: g(f(x)) if (exp(x)) else x
 do = lambda exp: lambda f: Z(lift_do(exp)(f))
 
-# Código traducido
 """
 
-    result_stmnt = 'nil'
-    print_vals = ''
-    print_lambdas = ''
+    result_stmnt = "nil"
+    print_vals = ""
+    print_lambdas = ""
     for i in range(len(variables)):
-        result_stmnt = f'cons(0)({result_stmnt})'
+        result_stmnt = f"cons(0)({result_stmnt})"
         print_vals = f"'x{i}' : x{i}, {print_vals}"
         print_lambdas = f"{print_lambdas}lambda x{i}:"
 
-
-    result_stmnt = f'result = program({result_stmnt})'
-    print_stmnt = f'print(apply({print_lambdas}'+"{"+print_vals+"})(result))"
-
-
+    result_stmnt = f"result = program({result_stmnt})"
+    print_stmnt = f"print(apply({print_lambdas}" + "{" + print_vals + "})(result))"
 
     # Escribe el archivo
     with open(output_filename, "w", encoding="utf-8") as f:
         f.write(preamble)
-        f.write(f"program = {translated_code}\n")
-        f.write(result_stmnt+"\n")
-        f.write(print_stmnt+"\n")
+        f.write(f"program = {translated_code}\n\n")
+        f.write(result_stmnt + "\n")
+        f.write(print_stmnt + "\n")
 
     return output_filename
 
@@ -170,10 +166,13 @@ def traduce_to_lambda(node, lambda_state=[], current_lambda=""):
         if tag == "Block":
             for var in node[1].symbols.keys():
                 lambda_state.append(var)
-            
-            return traduce_to_lambda(node[2][1:],lambda_state,''), lambda_state #El primer elemento tiene el tag declare y no lo necesitamos
-        
-        elif tag=="Asig":
+
+            return (
+                traduce_to_lambda(node[2][1:], lambda_state, ""),
+                lambda_state,
+            )  # El primer elemento tiene el tag declare y no lo necesitamos
+
+        elif tag == "Asig":
             changed_var = node[1][0]
 
             new_val = traduce_expression(node[2][0])
